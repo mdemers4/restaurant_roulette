1c6901428508e020e5db643f84cea25cc7f8c2af
Ian Bruce <brucegordonian@gmail.com>
2016-09-01 13:26:12 -0400

    Made index/new resveration views, reservation controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
 
-  resources :reservations, only: [:new, :create, :show]
+  resources :reservations, only: [:index, :new, :create]
 
-  resources :restaurants, only: [:new, :create :show]
+  resources :restaurants, only: [:new, :create, :show]
 
+  root 'reservations#index'
 end
