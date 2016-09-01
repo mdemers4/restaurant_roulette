ca738263c28ed7758ca7d1a5a9441083edc2178b
mitchell demers <mitchelldemers66@gmail.com>
2016-09-01 13:38:15 -0400

    added a reservation index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "reservations#index"
   resources :users, only: [:new, :create]
 
   resources :reservations, only: [:index, :new, :create]
