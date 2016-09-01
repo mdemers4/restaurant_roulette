9152acaebd36e85d27964d9fab9b9ea2d280ca0c
Rui Dong <rui.dong.jh@gmail.com>
2016-09-01 15:31:31 -0400

    Adding authorization to ensure users are logged in before they create a reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,15 +1,14 @@
 class ReservationsController < ApplicationController
+  before_action :ensure_logged_in, only: [:create, :destroy]
 
-def index
-  @reservation = Reservation.all
-end
+  def index
+    @reservation = Reservation.all
+  end
 
-def new
-  @reservation = Reservation.new
-end
+  def new
+    @reservation = Reservation.new
+  end
 
-def create
-end
-
-
+  def create
+  end
 end
