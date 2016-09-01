115d48092eb23a94173ceb8f13cb031984c2d36b
mitchell demers <mitchelldemers66@gmail.com>
2016-08-31 20:30:12 -0400

    db seed set up for neighbourhood and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Neighbourhood.create(
+# 	name: "Little Italy")
+
+# Neighbourhood.create(
+# 	name: "China Town")
+
+# Neighbourhood.create(
+# 	name: "Liberty Village")
+
+
+# Restaurant.create(
+# 	name: "papa's pizza", 
+# 	address: '123 collage st',
+# 	neighbourhood: 1 ,
+# 	user: 1
+# 	)
+# Restaurant.create(
+# 	name: "Gen Tzo", 
+# 	address: '123 spedina st',
+# 	neighbourhood: 2,
+# 	user: 2
+# 	)
+# Restaurant.create(
+# 	name: "hipster central", 
+# 	address: 'east liberty st ',
+# 	neighbourhood: 3,
+# 	user: 3
+# 	)
+
+
+# User.create(
+# 	name: "mitch",
+# 	email: "m@m.com",
+# 	phone: "9059059050",
+# 	password_digest: nil
+# 	)
+# User.create(
+# 	name: "a",
+# 	email: "a@m.com",
+# 	phone: "9059459050",
+# 	password_digest: nil
+# 	)
+# User.create(
+# 	name: "b",
+# 	email: "b@m.com",
+# 	phone: "9059259050",
+# 	password_digest: nil
+# 	)
+
+# Reservation.create(
+# 	time: nil,
+# 	date: nil,
+# 	size: 5)
+# Reservation.create(
+# 	time: nil,
+# 	date: nil,
+# 	size: 3)
+# Reservation.create(
+# 	time: nil,
+# 	date: nil,
+# 	size: 7)
